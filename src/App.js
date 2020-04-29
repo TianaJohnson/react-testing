72c3ecb49667f383b0e4ae6da0ef5d6853044e38
Tiana <Tiana.marie013@gmail.com>
2020-04-29 17:08:30 -0600

    Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from './component/header';
 
 
 function App() {
   return (
-    <div>
-      <p>Hello World</p>
+    <div className="App">
+      <Header/>
     </div>
   );
 }
