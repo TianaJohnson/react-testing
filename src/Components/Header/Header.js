2e5ec6bcbb25c6a4902270ff6045c6659c808b28
Tiana <Tiana.marie013@gmail.com>
2020-04-29 17:18:56 -0600

    Header Edit

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const Header = (props) => {
-    return(
-    <header>
+    return (
+        <header>
             Header
-    </header>
+        </header>
     )
 };
 
