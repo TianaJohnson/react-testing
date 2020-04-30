e65d0e7d8a82b26b107b6da14ad74dab73a036ac
Tiana <Tiana.marie013@gmail.com>
2020-04-30 13:26:15 -0600

    header logo

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './Header.scss';
-import Logo from './'
+import Logo from './../../assets/image/lblogo.jpg';
 
 const Header = (props) => {
     return (
         <header>
             <div className="wrapper">
                 <div className="logo">
-
+                    <img src={Logo} alt="logo"/>
                 </div>
             </div>
         </header>
